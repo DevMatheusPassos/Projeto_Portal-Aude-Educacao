c7e8f53a5d8459092d193cca8d589a607722fc62
DevMatheusPassos <devmatheuspassos@gmail.com>
2022-05-04 21:38:25 -0300

    Funcionalidade de ativar a opção e desativar a opção ativada anteriormente adicionada

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,58 @@
 let $menu__link = document.querySelectorAll(".menu__link");
-console.log($menu__link);
+let elementoAtualAtivado;
+let posicaoDoElementoAtual;
 
-$menu__link.forEach(function(element) {
+/* 
+* Verificação do elemento que está com a classe de ativo para chamar a função e colocar a imagem de ativo
+*/
+$menu__link.forEach(function(element, i) {
     if(element.classList.contains("--selected")) {
-        element.querySelector("img").setAttribute("src", "assets/images/header/nav-aprender-active.svg")
+        activeOption(i);
     }
 })
-for(var i = 0; i < $menu__link.length; i++) {
-    console.log($menu__link[i]);
-}
-
-let elementoAtualAtivado;
 
 function activeOption(n) {
     let img = $menu__link[n].querySelector("img");
+    
+    /* 
+    * Verificação do elemento atual que está ativado para remover a imagem de ativo
+    */
+    if(elementoAtualAtivado) {
+        var imgDoElementoAtual = elementoAtualAtivado.querySelector("img");
+        elementoAtualAtivado.classList.remove("--selected");
+        
+        if(posicaoDoElementoAtual === 0 && posicaoDoElementoAtual !== n) {
+            imgDoElementoAtual.setAttribute("src", "assets/images/header/nav-aprender.svg");
+        }
+        else if(posicaoDoElementoAtual === 1 && posicaoDoElementoAtual !== n) {
+            imgDoElementoAtual.setAttribute("src", "assets/images/header/nav-escola.svg");
+        }
+        else if(posicaoDoElementoAtual === 2 && posicaoDoElementoAtual !== n) {
+            imgDoElementoAtual.setAttribute("src", "assets/images/header/nav-diag360.svg");
+        }
+        else if(posicaoDoElementoAtual === 3 && posicaoDoElementoAtual !== n) {
+            imgDoElementoAtual.setAttribute("src", "assets/images/header/nav-profile.svg");
+        }
+    }
 
+    /* 
+    * Adicionar imagem de ativado ao elemento quando for clicado
+    */
     if(n === 0) {
-        img.setAttribute("src", "assets/images/header/nav-aprender-active.svg")
-        elementoAtualAtivado = $menu__link[0];
+        img.setAttribute("src", "assets/images/header/nav-aprender-active.svg");
     }
     else if(n === 1) {
-        img.setAttribute("src", "assets/images/header/")
-        elementoAtualAtivado = $menu__link[0];
+        img.setAttribute("src", "assets/images/header/nav-escola-active.svg");
     }
+    else if(n === 2) {
+        img.setAttribute("src", "assets/images/header/nav-diag360-active.svg");
+    }
+    else if(n === 3) {
+        img.setAttribute("src", "assets/images/header/nav-profile-active.svg");
+    }
+
+    $menu__link[n].classList.add("--selected");
+    elementoAtualAtivado = $menu__link[n];
+    posicaoDoElementoAtual = n;
+
 }
